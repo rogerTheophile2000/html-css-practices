735c372e8ed47c118ae5a3b9e3cee3a4568a3484
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-07 19:08:58 +0300

    whac-a-mole

---
+++ whac a mole/main.js
@@ -0,0 +1,5 @@
+const scoreDisplay = document.querySelector("#score");
+const timeLeftDisplay = document.querySelector("#time-left");
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+
