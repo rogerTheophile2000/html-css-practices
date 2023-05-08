be3dd803ffe848d3bbfd95f16127602829b4d240
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-08 03:50:48 +0300

    adding whac-a-mole game

--- whac a mole/main.js
+++ whac a mole/main.js
@@ -1,5 +1,52 @@
-const scoreDisplay = document.querySelector("#score");
-const timeLeftDisplay = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+const timeLeft = document.querySelector("#time-left");
 const squares = document.querySelectorAll(".square");
 const mole = document.querySelectorAll(".mole");
 
+let result = 0;
+let  hitPosition;
+let currentTime = 60;
+let timeId = null;
+
+function randomSquare() {
+    squares.forEach(square => {
+      square.classList.remove('mole')
+    })
+  
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+  
+    hitPosition = randomSquare.id
+  }
+  
+  squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+      if (square.id == hitPosition) {
+        result++
+        score.textContent = result
+        hitPosition = null
+      }
+    })
+  })
+  
+  function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+  }
+  
+  moveMole()
+  
+  function countDown() {
+   currentTime--
+   timeLeft.textContent = currentTime
+  
+   if (currentTime == 0) {
+     clearInterval(countDownTimerId)
+     clearInterval(timerId)
+     alert('GAME OVER! Your final score is ' + result)
+   }
+  
+  }
+  
+  let countDownTimerId = setInterval(countDown, 1000)
+  
+  
