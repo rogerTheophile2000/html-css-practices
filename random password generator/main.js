212db7f3f9f38bcce8cef233803ff7155fcfda8d
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-08 05:25:37 +0300

    adding password generator

---
+++ random password generator/main.js
@@ -0,0 +1,34 @@
+const passwordBox = document.querySelector("#password");
+const create_pass = document.querySelector("#create_pass");
+const copyPass = document.querySelector("#btn-cpy")
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789"
+const symbol = "@#$%^&*()_+-=[]{}|/><";
+
+const allChars = upperCase + lowerCase + number +symbol;
+
+create_pass.addEventListener("click", createPassword);
+copyPass.addEventListener("click", copyPassword);
+
+console.log(allChars);
+
+function createPassword(){
+    let password = ""
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() *   number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
